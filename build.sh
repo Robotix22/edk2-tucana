1d81569fc5f95a3b639f67230408ee22f559be2f
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2022-03-31 14:43:56 +0200

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# based on the instructions from edk2-platform
+set -e
+. build_common.sh
+# 编译前需清除残留
+
+rm -rf workspace/Build/tucanaPkg/DEBUG_GCC5/FV/Ffs/7E374E25-8E01-4FEE-87F2-390C23C606CDFVMAIN
+# not actually GCC5; it's GCC7 on Ubuntu 18.04.
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p tucanaPkg/tucanaPkg.dsc
+gzip -c < Build/tucanaPkg/DEBUG_GCC5/FV/TUCANAPKG_UEFI.fd >uefi_image
+cat tucana.dtb >>uefi_image
+abootimg --create uefi.img -k uefi_image -r ramdisk-null -f bootimg.cfg
