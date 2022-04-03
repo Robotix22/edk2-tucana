3402a6081ba97efe349703346d16a64638b85e8d
Robotix22 <98209628+Robotix22@users.noreply.github.com>
2022-04-03 16:57:54 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # based on the instructions from edk2-platform
+rm -r Build
+rm uefi.img
+rm uefi_image
 set -e
 . build_common.sh
-# 编译前需清除残留
 
 rm -rf workspace/Build/tucanaPkg/DEBUG_GCC5/FV/Ffs/7E374E25-8E01-4FEE-87F2-390C23C606CDFVMAIN
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
